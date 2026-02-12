455c03d7dd952f59a7f61b0f806b5ff8a1900d6b
Aayan-coder20 <khnaayan07@gmail.com>
2026-02-12 20:40:28 +0530

    Revise comments with coder name and date
    Updated coder name and date in the comments.

--- main.py
+++ main.py
@@ -1,8 +1,27 @@
 # AIM: Develop a Python program that reads a text file and 
 # prints words of specified lengths (e.g., three, four, 
 # five, etc.) found within the file.
-# Coder:
-# Date:
+# Coder: Aayan Khan
+# Date:11-02-2026
 
-print("--- Extracting Words from Text File ---\n")
+print("--- Extracting Words from Text File ---")
+
+n = int(input("\nEnter Length of Words: "))
+
+f = open("story.txt", "r")
+text = f.read()
+f.close()
+
+words = text.lower().split()
+unique = []
+
+for w in words:
+    if len(w) == n and w not in unique:
+        unique.append(w)
+
+unique.sort()
+
+print("\nWords with length", n, "are:", unique)
+
+
 
